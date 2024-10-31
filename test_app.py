64a418d204c14d2af039ae07557d0ce05b0ae9de
Andrey Kuznetsov <5andry8831@gmail.com>
2024-10-31 18:45:35 +0300

    add tests, change layout

---
+++ test_app.py
@@ -0,0 +1,32 @@
+import asyncio
+import unittest
+
+from fastapi.testclient import TestClient
+
+from app import app
+from db import get_all_sessions
+
+
+class TestApp(unittest.IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(app)
+
+    def test_read_root(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    async def test_websocket(self):
+        with self.client.websocket_connect('/ws') as websocket:
+            websocket.send_json({"action": "connect", "user_agent": "navigator.userAgent"})
+            data = websocket.receive_json()
+            await asyncio.sleep(1)
+            self.assertEqual(1, len(get_all_sessions()))
+
+            websocket.send_json({"action": "disconnect", "session_id": data['session_id']})
+            await asyncio.sleep(1)
+            self.assertEqual(0, len(get_all_sessions()))
+
+
+if __name__ == '__main__':
+    unittest.main()
